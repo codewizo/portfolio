59173e758a47052bce35efa9dfafa0c84970872f
codewizo <ongliberius@gmail.com>
2023-08-05 22:29:08 +0300

    Onclick Function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,19 @@
-function show(){
-    const navlist = document.querySelector("#navbar ul li");
-    navlist.style.display = "";
-    navlist.style.height = "150px";
+function displayNavBar(){
+    let navbars = document.getElementById("nav-toggler");
+    let navlist = document.querySelector("#navbar ul");
+
+    function show(){
+        navlist.children[0].style.display = "block";
+        navlist.children[1].style.display = "block";
+        navlist.children[2].style.display = "block";
+        navlist.children[3].style.display = "block";
+        navlist.children[4].style.display = "block";
+        navlist.style.transition ="all 1s ease";
+        setTimeout(hide, 6000);
+    }
+    
+    function hide(){
+        navlist.style.display = "none";
+    }
+    navbars.onclick = show();
 }
