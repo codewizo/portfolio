fde6e31107ba0e19f5016bbbea2c09858791fca7
codewizo <ongliberius@gmail.com>
2023-08-05 16:56:13 +0300

    Responsive Navbar JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,5 @@
-document.getElementsByClassName('')
+function show(){
+    const navlist = document.querySelector("#navbar ul li");
+    navlist.style.display = "";
+    navlist.style.height = "150px";
+}
