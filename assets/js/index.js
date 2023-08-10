1bad9914f9bda7dd17ab872944d1da765fbe16d9
codewizo <ongliberius@gmail.com>
2023-08-10 23:02:29 +0300

    Responsive Navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
+//const bar = document.getElementsByClassName("bar");
+//const navigation = document.getElementsByClassName("dropdown-menu");
+function show(){
+    document.querySelector(".dropdown-menu").style.display = "flex";
+    document.querySelector(".dropdown-menu").style.transition = "all 0.5s ease";
+    setTimeout(hide, 4000);
+}
+function hide(){
+    document.querySelector(".dropdown-menu").style.display = "none";
+}
+
 
