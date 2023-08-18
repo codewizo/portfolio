66abe4cf9c9a3cd3f88b1748532686f7684cc291
codewizo <ongliberius@gmail.com>
2023-08-18 23:53:30 +0300

    Javascript navigation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 const bar = document.getElementById("nav-toggler");
 const menu = document.querySelector(".dropdown-menu");
-//const navigation = document.getElementsByClassName("dropdown-menu");
+const navigation = document.getElementsByClassName("dropdown-menu");
 function show(){
     if (menu.style.display = "flex"){
         bar.onclick = menu.style.display = "none";
